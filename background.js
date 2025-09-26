ffd1d613324a5dc43aa5f6145cfacba4ad09c320
Ahmad Luqman <163151037+Ahmad-Luqman0@users.noreply.github.com>
2025-09-26 10:54:42 +0500

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js (required by manifest MV3 background.service_worker)
-self.addEventListener('install', () => {
-  console.log('[background] installed');
-});
-self.addEventListener('activate', () => {
-  console.log('[background] activated');
-});
